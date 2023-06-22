8e0beba44dbb83a270f4ee736bd1eafdf7078a3d
Bartłomiej Górkiewicz <bartek@doyensec.com>
2023-06-22 03:16:55 +0200

    aa

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import <h1>aaA</h1> Vue from 'vue'
+import <pre><code><h1>I'm big BAD HTML</h1><script>alert("your hacked");</script></code></pre> Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Gallerry from './views/Gallerry.vue'
